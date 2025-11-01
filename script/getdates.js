afdba544969e6244be3033898086a63a86fca308
sayhitoritu <rrai@byupathway.edu>
2025-11-01 08:09:11 +0530

    update

---
+++ script/getdates.js
@@ -0,0 +1,6 @@
+// Display current year dynamically
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+// Display last modified date dynamically
+document.getElementById("lastModified").textContent =
+  "Last Modification: " + document.lastModified;
